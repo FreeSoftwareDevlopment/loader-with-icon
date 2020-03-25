3645645e2a58f92ec9645b4b9d9b437d1be04154
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-03-25 17:13:59 +0100

    Update load.js

--- load.js
+++ load.js
@@ -17,5 +17,14 @@ var loader=(function(){
 					document.getElementById("load").style="display:none";
 				}
 			}, 3);
-			return function(){if(!ends){ends=true;console.info("Loader Disabled");}else{console.warn("Loader is Disabled!");}}
+			function enabless(){document.getElementById("load").style="display:block";var intarvals=setInterval(function(){
+				idd-=0.5;
+				imagess.style='-webkit-transform:rotate('+idd+'deg);  -moz-transform: rotate('+idd+'deg);  -ms-transform: rotate('+idd+'deg);  -o-transform: rotate('+idd+'deg);  transform: rotate('+idd+'deg);height:90px;top:calc(50% - (90px / 2));position:fixed;left:50%;';
+				imagessab.style='-webkit-transform:rotate('+(idd*2)+'deg);  -moz-transform: rotate('+(idd*2)+'deg);  -ms-transform: rotate('+(idd*2)+'deg);  -o-transform: rotate('+(idd*2)+'deg);  transform: rotate('+(idd*2)+'deg);height:90px;top:calc(50% - (90px / 2));position:fixed;left:50%;';
+				if(ends){
+					clearInterval(intarvals);
+					document.getElementById("load").style="display:none";
+				}
+			}, 3);}
+			return function(){if(!ends){ends=true;console.info("Loader Disabled");}else{console.info("Start Loader");ends=false;enabless();}}
 		})();
